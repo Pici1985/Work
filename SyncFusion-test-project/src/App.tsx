f461642255fc6a9cc282457092ac89a5221fd1e4
Balazs Ferencz <balazs.ferencz@clearskylogic.com>
2024-11-27 11:49:28 +0000

    initial commit with the component working

--- SyncFusion-test-project/src/App.tsx
+++ SyncFusion-test-project/src/App.tsx
@@ -1,21 +1,56 @@
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import { registerLicense } from "@syncfusion/ej2-base";
+import {
+  ColumnDirective,
+  ColumnsDirective,
+  TreeGridComponent,
+} from "@syncfusion/ej2-react-treegrid";
+import { sortData } from "./sampleData/data";
 
-registerLicense("Replace your generated license key here");
+registerLicense(import.meta.env.VITE_SYNCFUSION_LICENSE_KEY);
 
 function App() {
   return (
     <>
       <div>
         <h1>SyncFusion test Project</h1>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <p>TreeGridComponent</p>
+        <div>
+          <TreeGridComponent
+            dataSource={sortData}
+            treeColumnIndex={1}
+            childMapping="subtasks"
+          >
+            <ColumnsDirective>
+              <ColumnDirective
+                field="Category"
+                headerText="Category"
+                width="150"
+              />
+              <ColumnDirective
+                field="orderName"
+                headerText="Order Name"
+                width="170"
+              />
+              <ColumnDirective
+                field="orderDate"
+                headerText="Order Date"
+                width="130"
+                format="yMd"
+                textAlign="Right"
+                type="date"
+              />
+              <ColumnDirective
+                field="price"
+                headerText="Price"
+                width="100"
+                textAlign="Right"
+                type="number"
+                format="C0"
+              />
+            </ColumnsDirective>
+          </TreeGridComponent>
+        </div>
       </div>
     </>
   );
